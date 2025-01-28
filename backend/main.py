85e94d04360ea9cb20b1c2a16862c7c229c7f37a
Ravi Garg <ravigargrud@gmail.com>
2025-01-28 16:03:13 +0530

    minor additions

--- backend/main.py
+++ backend/main.py
@@ -6,12 +6,19 @@ from routes.ai import router as aiRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 
-
 app = FastAPI(
     title="Video Sharing API",
     description="A simple API to share videos",
     version="0.1",
     summary="Video Sharing API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
 )
 
 app.include_router(userRouter)
